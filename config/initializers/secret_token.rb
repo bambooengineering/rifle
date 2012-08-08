ff166f6143e239a2a3ed622d0b9d1399ac21741e
Harry Lascelles <harry@firstbanco.com>
2012-08-08 10:58:29 +0100

    Removing excess docs

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,1 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
 Rifle::Application.config.secret_token = 'fadsl984l4an0ckm2109dm1mx89453f7ylscms98i42cnlk210iznsfdl293fjrlf8c4m3w4joqfjds4q2o3ejflo3j4fdm2998mcjdq390spoq2e28m8m710zmd32d5'
